edb5332c1c4ed3f47d1319453ab4af39e37cce92
Nasim Shiroodi <nasim.breeze97@gmail.com>
2024-03-22 18:53:15 +0330

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Frontendnasim" target="_blank">
+            Nasim Shiroodi
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/Frontendnasim/react-weather-app">
+            open-sourced on Github
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -5,34 +5,41 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-9">
             <input
               type="text"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input type="submit" value="Search" className="submit-btn" />
           </div>
         </div>
       </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Friday 3.00</li>
-        <li>Clear</li>
+      <h1 className="city">New York</h1>
+      <ul className="city-info">
+        <li className="city-details">Friday 3.00</li>
+        <li className="city-details">Clear</li>
       </ul>
-      <div className="row">
+      <div className="row mb-5">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-            alt="Clear"
-          />
-          -1°C
+          <div className="clearfix d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Clear"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">-1</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
-          <ul>
+          <ul className="weather-info">
             <li>Precipitation: 0%</li>
             <li>Humidity: 42%</li>
             <li>Wind: 3 m/s</li>
