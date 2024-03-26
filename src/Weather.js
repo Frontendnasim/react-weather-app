3fa4b882cd286164a70f1641726a708765c877a5
Nasim Shiroodi <nasim.breeze97@gmail.com>
2024-03-26 19:34:20 +0330

    Created WeatherForecast component

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import "./Weather.css";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 import { RotatingLines } from "react-loader-spinner";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -56,6 +57,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
