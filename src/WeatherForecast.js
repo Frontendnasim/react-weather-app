6f4e58c8821abb32f0fea9e5e8e1b4d83095653e
Nasim Shiroodi <nasim.breeze97@gmail.com>
2024-03-27 17:22:06 +0330

    Added API integration

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,21 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  console.log(props);
+
+  let apiKey = "36b1aa2d00565t7424oa4399fb630c11";
+  let longitude = props.coordinates.longitude;
+  let latitude = props.coordinates.latitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
